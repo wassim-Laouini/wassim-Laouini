6d5ae2983ef4b46c1ce5d582c5770dff011f8012
wassim laouini <wassimlaouini02@gmail.com>
2022-05-15 19:25:58 +0200

    body type texte

--- database/migrations/2022_05_15_171408_about_us.php
+++ database/migrations/2022_05_15_171408_about_us.php
@@ -17,7 +17,7 @@ return new class extends Migration
         Schema::create('about_us', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('body');
+            $table->text('body');
             $table->string('status');
             $table->timestamps();
             
