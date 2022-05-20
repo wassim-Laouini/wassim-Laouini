98cfd35290ee2f77fa776d9ce9f526ca3b964100
fakherbbh <mabroukfakher007@gmail.com>
2022-05-20 16:23:00 -0700

    home section amd services section almost done
    mazel fema shway detail sahliin

--- front_end_web/src/layouts/Main/MainComponenets/TopBar.jsx
+++ front_end_web/src/layouts/Main/MainComponenets/TopBar.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import "./componentsStyle.css";
 
 function TopBar() {
-    return <div>TopBar</div>;
+    return(
+<div  >  </div>
+
+);
 }
 
 export default TopBar;
--- front_end_web/src/screens/Home/Home.jsx
+++ front_end_web/src/screens/Home/Home.jsx
@@ -27,37 +27,134 @@ function Home() {
     // const data = ["r", "s"];
 
     return (
-        //* start of home
+//* home section & services section
+<div className="home-container" >
+{/***** start of home section 1   *****/}
 
-        <div className="home-area">
-            <div className="home-content">
+       <div className="home-section1">
 
+                <div className="home-content">
+                    <h1> Ready   <span>For The Future.</span>  </h1>
+                    {/***** 2 types of image loading  *****/}
+                    {/* <img src={require("../../images/Asset_26.png")} alt="" /> */}
+                    {/* <img src={image} alt="" /> */}
 
-                <h1>
-                    Ready <span>For The Future.</span>
-                </h1>
-                {/***** 2 types of image loading  *****/}
-                {/* <img src={require("../../images/Asset_26.png")} alt="" /> */}
-                {/* <img src={image} alt="" /> */}
-
-                <div className="container-btn">
-                    <button className="btn1"> About Us</button>
-                    <button className="btn2"> Contact Us</button>
+                    <div className="container-btn">
+                       <button className="btn1"> About Us</button>
+                        <button className="btn2"> Contact Us</button>
+                    </div>
                 </div>
-            </div>
+{/***** the bottom wave shope   *****/}
+                <div className="wave-shape">
+                    <img src={require("../../images/Asset_2.png")} alt="" />
+                </div>
+{/***** end of the wave shope   *****/}
 
-            {/* font usage type */}
-            {/* <p
-                style={{
-                    fontFamily: "commonFont",
-                    color: "white",
-                    marginLeft: "20px",
-                }}
-            >
-                hey
-            </p> */}
-            {/* <p className="pTag">heyyy again</p> */}
+{/***** Upwork /email/ whatsapp Icons    *****/}
+                <div className="contact-icons">
+                    <a target="blank" href="email">
+                        {" "}
+                        <img
+                            className="email-icon"
+                            src={require("../../images/email.png")}
+                            alt=""
+                        />{" "}
+                    </a>
+                    <a target="blank" href="phone">
+                        {" "}
+                        <img
+                            className="phone-icon"
+                            src={require("../../images/phone.png")}
+                            alt=""
+                        />{" "}
+                    </a>
+                    <a target="blank" href="upwork">
+                        {" "}
+                        <img
+                            className="upwork-icon"
+                            src={require("../../images/upwork.png")}
+                            alt=""
+                        />{" "}
+                    </a>
+                </div>
+{/*****  End of  Upwork /email/ whatsapp Icons    *****/}
+
         </div>
+{/***** end of home section 1*  *****/}
+
+
+
+{/***** start of services section  *****/}
+<div className="services">
+<h1 className="services-heading" > Our Services </h1>
+<div className="services-wrapper">
+<div className="services-component">
+<div className="services-icon"> <img style={{width:"40px"}}  src={require("../../images/web dev.png")} alt="" />    </div>
+<div className="services-text"> <h3> Web Development</h3>
+
+<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur expedita reprehenderit laudantium repellendus qui ab tenetur, aliquam facere veritatis autem adipisci maxime ullam iste magnam eaque esse cupiditate obcaecati repellat?   </p>
+</div>
+<a href="link"> Read More  </a>
+
+</div>
+<div className="services-component">
+<div className="services-icon"> <img  style={{width:"40px"}} src={require("../../images/music.png")} alt="" />    </div>
+<div className="services-text"> <h3> IT Solution</h3>
+
+<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur expedita reprehenderit laudantium repellendus qui ab tenetur, aliquam facere veritatis autem adipisci maxime ullam iste magnam eaque esse cupiditate obcaecati repellat?   </p>
+</div>
+<a href="link"> Read More  </a>
+
+</div>
+<div className="services-component">
+<div className="services-icon"> <img style={{width:"40px"}}  src={require("../../images/scraping.png")} alt="" />    </div>
+<div className="services-text"> <h3> IT Solution</h3>
+
+<p>  Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur expedita reprehenderit laudantium repellendus qui ab tenetur, aliquam facere veritatis autem adipisci maxime ullam iste magnam eaque esse cupiditate obcaecati repellat?  </p>
+</div>
+<a href="link"> Read More  </a>
+
+</div>
+<div className="services-component">
+<div className="services-icon"> <img  style={{width:"40px"}} src={require("../../images/security.png")} alt="" />    </div>
+<div className="services-text"> <h3> IT Solution</h3>
+
+<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur expedita reprehenderit laudantium repellendus qui ab tenetur, aliquam facere veritatis autem adipisci maxime ullam iste magnam eaque esse cupiditate obcaecati repellat?   </p>
+</div>
+<a href="link"> Read More  </a>
+
+</div>
+<div className="services-component">
+<div className="services-icon"> <img style={{width:"40px"}} src={require("../../images/mobile.png")} alt="" />    </div>
+<div className="services-text"> <h3> IT Solution</h3>
+
+<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur expedita reprehenderit laudantium repellendus qui ab tenetur, aliquam facere veritatis autem adipisci maxime ullam iste magnam eaque esse cupiditate obcaecati repellat?   </p>
+</div>
+<a href="link"> Read More  </a>
+
+</div>
+<div className="services-component">
+<div className="services-icon"> <img style={{width:"40px"}}  src={require("../../images/disgne icon.png")} alt="" />    </div>
+<div className="services-text"> <h3> Design</h3>
+
+<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur expedita reprehenderit laudantium repellendus qui ab tenetur, aliquam facere veritatis autem adipisci maxime ullam iste magnam eaque esse cupiditate obcaecati repellat?   </p>
+</div>
+<a href="link"> Read More  </a>
+
+</div>
+
+
+
+
+
+
+</div>
+</div>
+{/***** End of services section  *****/}
+
+{/* <a href="ss"> <img className="arrow" src={require("../../images/up-arrow.png")} alt="" /> </a> */}
+
+      </div>
     );
 }
 
